c5fa3b4224747f89395d4ec2aa9ea171cb8bbc9b
Instructor <dodyDev311@gmail.com>
2024-08-18 08:22:48 +0530

    search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import UserList from "./Components/UserList";
+import FilterUsers from "./Components/FilterUsers";
+
+import {Routes, Route} from "react-router-dom";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+            {/* <Route path="/" element={<UserList />} />
+            <Route path="user/:x" element={<UserList />} /> */}
+            <Route path="/" element={<FilterUsers />} />
+            {/* usesearchParams() => ? */}
+            {/* {useparams() => varibale} */}
+        </Routes>
     </div>
   );
 }
